fc8a515c12771f7b84e6ed632dd6a2dcfd256c01
Gunasekar Jabbala <gunasekar.jabbala@gmail.com>
2025-03-15 10:54:00 +0000

    added likes functionality

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,18 @@
-
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from .forms import NotesForm
 from .models import Notes
+
+def add_like_view(request, pk):
+    if request.method == "POST":
+        note = get_object_or_404(Notes, pk=pk)
+        note.likes += 1
+        note.save()
+        return HttpResponseRedirect(reverse("notes.detail", args=(pk,)))
+    return Http404
 
 class NotesCreateView(CreateView):
     model = Notes
